d1cbd0910febe0d33d54d737803b25b88ae9541a
xaman <xaman282@gmail.com>
2025-04-25 04:25:27 +0530

    i'll never stop

--- problem_solving/First_java.java
+++ problem_solving/First_java.java
@@ -1,8 +1,10 @@
 package problem_solving;
 
+import java.io.FilterOutputStream;
 import java.util.Scanner;
 
 public class First_java {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 //        float a = sc.nextInt();// here is automatic type casting because a destination type is larger
@@ -38,18 +40,16 @@ public class First_java {
         // value gets promote into an int type because int is bigger than byte
 //        System.out.println(sum);
   //// Q1. Write a program to print whether a number is even or odd, also take input from the user.
-//        System.out.println("enter your number");
-//        int a = sc.nextInt();
-//        if(a%2==0){
-//            System.out.println(a+" is even");
-//        }else{
-//            System.out.println(a+" is odd");
-//        }
+//        System.out.println("enter your number for checking it is even or odd");
+//        int n = sc.nextInt();
+//        evenodd(n);
+
 
         /// Q2. Take the name as input and print a greeting message for that particular name.
 //        System.out.println("Enter your Name");
 //        String name = sc.nextLine();
-//        System.out.println("Hello "+ name + " welcome to my house.");
+//        getName(name);
+//
 
         ///Q3. Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
 //        System.out.println("enter the principal amount");
@@ -57,10 +57,9 @@ public class First_java {
 //        System.out.println("enter the time consumed");
 //        int t = sc.nextInt();
 //        System.out.println("enter the rate");
-//        float r = sc.nextFloat();
-//
-//        float result = (p*r*t)/100;
-//        System.out.println("The simple intrest is: "+result);
+//        int r = sc.nextInt();
+//        System.out.println("SI :- "+simpleInterest(p,t,r));
+
 
 /// /// Q4. Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
 
@@ -68,87 +67,152 @@ public class First_java {
 //        int a = sc.nextInt();
 //        System.out.println("Enter the second number");
 //        int b = sc.nextInt();
-//
-//        System.out.println("Enter the operator");
-//        char operator = sc.next().trim().charAt(0);
-//
-//        if(operator == '+'){
-//            int res = a+b;
-//            System.out.println(res);
-//        } else if (operator == '-') {
-//            int res = a - b;
-//            System.out.println(res);
-//        } else if (operator == '*') {
-//            int res =a*b;
-//            System.out.println(res);
-//        } else if (operator == '/') {
-//            if(b!=0){
-//                float res = (float)(a)/(float)(b);
-//                System.out.println(res);
-//            }
-//        }
-//        else {
-//            System.out.println("please enter the right operation (+,-,*,/)");
-//        }
-
+//        int fnalres = calculate(a,b);
+//        System.out.println(fnalres);
 
         /// Q5. Take 2 numbers as input and print the largest number.
 //        System.out.println("Enter the first number");
 //        int a = sc.nextInt();
 //        System.out.println("Enter the second number");
 //        int b = sc.nextInt();
-//
-//        if(a>b){
-//            System.out.println(a+" is greater than "+b);
-//        }else {
-//            System.out.println(b+" is greater than "+a);
-//        }
+//        greater(a,b);
+
 
         /// Q6. Input currency in rupees and output in USD.
 //        System.out.println("Enter amount in rupees");
 //        float rs = sc.nextFloat();
-//
-//        double usd = (rs)*(0.012);
-//        System.out.println(rs+" into USD $"+usd);
+//        double res = currencyConverter(rs);
+//        System.out.println("$" + res);
+
 
         /// Q7. To calculate Fibonacci Series up to n numbers.
 //        System.out.println("Enter nth number for Fibonacci series");
 //        int n = sc.nextInt();
-//
-//        int a = 0;
-//        int b = 1;
-//        /// /0,1,1,2,3,5,.......fibonacci series
-//        for(int i = 1;i<=n;i++){
-//
-//           int c = a+b;
-//            a = b;
-//            b = c;
-//            System.out.print(c+" ");
-//
-//
-//        }
+//        fibonacci(n);
+
+
 
 
         /// Q8. To find out whether the given String is Palindrome or not.
 //        System.out.println("Enter Your string");
 //        String input = sc.nextLine();
-//
-//        int length = input.length();
-//        boolean is_palindrome = true;
-//
-//        for(int i = 0;i<length/2;i++){
-//            if(input.charAt(i)!= input.charAt(length-1-i)){
-//                is_palindrome = false;
-//                break;
-//            }
-//
-//        }
-//        if (is_palindrome){
-//            System.out.println(input+" is palindrome");
-//        }else {
-//            System.out.println(input+" is not palindrome");
-//        }
+//        palindrome(input);
 
 
+        System.out.println("Enetr 3 digit number ");
+        int n = sc.nextInt();
+
+
+        boolean res = armstrong(n);
+        if(res == true){
+            System.out.println("it is an Armstrong");
+        }else{
+            System.out.println("it's not an Armstrong");
+        }
+
+
+
+    }
+    public static void evenodd(int n){
+        if (n%2==0){
+            System.out.println(n+" is even");
+        }else {
+            System.out.println(n+" is odd");
+        }
+
+
+    }
+    public static void getName(String name){
+        System.out.println("Hello "+name+" welcome to my house");
+    }
+    public static int simpleInterest( int p,int t,int r){
+        int res = (p*r*t)/100;
+        return res;
+    }
+    public static int calculate(int a , int b){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the operator");
+        char operator = sc.nextLine().charAt(0);
+       if(operator == '+'){
+           int res = a+b;
+           return res;
+
+       }else if (operator == '-'){
+
+           int res = a-b;
+           return res;
+       }
+       else if (operator == '*'){
+           int res = a*b;
+           return res;
+       }
+       else if (operator == '/'){
+           if(b!=0) {
+               int res = a/b;
+               return res;
+           }
+       }
+       else {
+           System.out.println("enter the correct operator 🙂");
+       }
+     return 0;
+    }
+public static  void greater (int a , int b){
+
+        if(a>b){
+            System.out.println(a+" is greater than "+b);
+        }else {
+            System.out.println(b+" is greater than "+a);
+        }
+}
+public  static double currencyConverter(float rs){
+        double res = rs*0.012;
+        return res;
+}
+public static void fibonacci (int n ){
+    int a = 0;
+    int b = 1;
+    /// /0,1,1,2,3,5,.......fibonacci series
+    for(int i = 1;i<=n;i++){
+
+        int c = a+b;
+        a = b;
+        b = c;
+        System.out.println(c+" ");
+    }
+}
+public static void palindrome(String input){
+    int length = input.length();
+    boolean is_palindrome = true;
+
+    for(int i = 0;i<length/2;i++){
+        if(input.charAt(i)!= input.charAt(length-1-i)){
+            is_palindrome = false;
+            break;
+        }
+
+    }
+    if (is_palindrome){
+        System.out.println(input+" is palindrome");
+    }else {
+         System.out.println(input+" is not palindrome");
+    }
+}
+    public static boolean armstrong(int n) {
+        int original = n; // Original value save kar lo
+        int sum = 0;
+
+        while (n > 0) { // `for` loop ki jagah `while` loop use karo
+            int rem = n % 10;
+            int pow = rem * rem * rem;
+            sum += pow;
+            n = n / 10; // Number ko divide karte raho
+        }
+
+        if (sum == original) { // Original value ke saath compare karo
+            return true;
+        } else {
+           return false;
+        }
     }
 }
