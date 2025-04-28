85660ac31fd487933951990bb0524445d657ecf0
xaman <xaman282@gmail.com>
2025-04-29 02:34:51 +0530

    i'll never stop

--- problem_solving/array_based.java
+++ problem_solving/array_based.java
@@ -5,26 +5,52 @@ import java.util.Arrays;
 public class array_based {
     public static void main(String[] args) {
 
-        System.out.println(anagram("aman","namb"));
 
-    }
-    public static Boolean anagram ( String s, String t){
-        String st1 = s.replaceAll("//s"," ");
-        String st2 = t.replaceAll("//s"," ");
+        ////=================== Anagram ===============================////////
+//        System.out.println(anagram("aman","nama"));
 
-        if(st1.length() != st2.length()){
-            return  false;
-        }
-        else{
-            char [] c1 = st1.toLowerCase().toCharArray();
-            char [] c2 = st2.toLowerCase().toCharArray();
+        char[] ar = new char[6];
 
-            Arrays.sort(c1);
-            Arrays.sort(c2);
+        ar[0]= 'a';
+        ar[1]= 'b';
+        ar[2]= 'c';
+        ar[3]= 'd';
+        ar[4]= 'e';
 
-            return Arrays.equals(c1,c2);
+        insert(ar, 2, 'f');
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i]);
         }
 
     }
+//    public static Boolean anagram ( String s, String t){
+//        String st1 = s.replaceAll("//s"," ");
+//        String st2 = t.replaceAll("//s"," ");
+//
+//        if(st1.length() != st2.length()){
+//            return  false;
+//        }
+//        else{
+//            char [] c1 = st1.toLowerCase().toCharArray();
+//            char [] c2 = st2.toLowerCase().toCharArray();
+//
+//            Arrays.sort(c1);
+//            Arrays.sort(c2);
+//
+//            return Arrays.equals(c1,c2);
+//        }
+//
+//    }
+
+
+    /// /// ============== inserting an element in an array at desired position =================///////
+
+    public static void insert(char[] arr, int pos, char val) {
+        for (int i = arr.length - 1; i >= pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[pos-1] = val;
+    }
+
 
 }
