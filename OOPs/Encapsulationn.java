6ba25b42863d1b414ac9f48dc3c057c2e812b9b2
Aman Singh <xaman282@gmail.com>
2025-10-02 22:46:24 +0530

    started again

---
+++ OOPs/Encapsulationn.java
@@ -0,0 +1,26 @@
+package OOPs;
+
+public class Encapsulationn {
+
+    public static class Studnet {
+        private String name;
+
+        public void setName(String name){
+            if(!name.isEmpty() && name != null){
+                this.name = name;
+            }
+        }
+        public String getName(){
+            return this.name;
+        }
+
+    }
+    public static void main(String[] args) {
+        Studnet s1 = new Studnet();
+        s1.setName("Aman");
+        System.out.println(s1.getName());
+
+
+    }
+
+}
--- OOPs/OOps_basic.java
+++ OOPs/OOps_basic.java
@@ -1,5 +1,7 @@
 package OOPs;
 
+import java.util.Arrays;
+
 public class OOps_basic {
 
     ///  class is a user defined blueprint through an object is created
@@ -10,10 +12,14 @@ public class OOps_basic {
     }
     public static void main(String[] args) {
         Student s1 = new Student(); // Declaration of an object through Student class
-
+        ///  Object : - Class ka real-world instance. Har object ka apna state (data) aur behavior (methods) hota hai.
         s1.name = "Aman";  // initialization
         s1.Roll_no = 138018; // initialization
         s1.department = "BTech CSE"; // initialization
+        System.out.println("Student Details:");
+        System.out.println("Name: "+s1.name);
+        System.out.println("Roll No: "+s1.Roll_no);
+        System.out.println("Department: "+s1.department);
 
 
 
